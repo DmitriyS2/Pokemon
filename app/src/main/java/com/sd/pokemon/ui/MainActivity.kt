a8f0038f91b13514d44d30785364520ae376eed8
Dmitriy <dim116@inbox.ru>
2024-05-08 09:49:38 +0300

    Initial commit

---
+++ app/src/main/java/com/sd/pokemon/ui/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.sd.pokemon.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sd.pokemon.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
